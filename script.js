9891f3399a64ebf78605dbf97b6adf15ee5d9b16
Ashok814B <bollimunthaashok814@gmail.com>
2025-04-07 02:03:02 +0530

    Done with all things the. The Game is Finished... But need to adjust some resuable code and init() function

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const btnhold = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentscore = 0;
 let active = 0;
+let playing = true;
 
 //starting conditions
 score0El.textContent = 0;
@@ -32,38 +33,76 @@ const switchplayer = function () {
 
 //Rooling dice functionality
 btnroll.addEventListener('click', function () {
-  //Generating random Number
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //Generating random Number
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //Display Diced
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //Display Diced
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //Check rolled
-  if (dice != 1) {
-    //add dice to current score
-    currentscore += dice;
-    document.getElementById(`current--${active}`).textContent = currentscore;
-  } else {
-    //
-    // currentscore = 0;
-    // document.getElementById(`current--${active}`).textContent = 0;
-    // active = active === 0 ? 1 : 0;
-    // player0El.classList.toggle('player--active');
-    // player1El.classList.toggle('player--active');
+    //Check rolled
+    if (dice != 1) {
+      //add dice to current score
+      currentscore += dice;
+      document.getElementById(`current--${active}`).textContent = currentscore;
+    } else {
+      //
+      // currentscore = 0;
+      // document.getElementById(`current--${active}`).textContent = 0;
+      // active = active === 0 ? 1 : 0;
+      // player0El.classList.toggle('player--active');
+      // player1El.classList.toggle('player--active');
 
-    switchplayer();
+      switchplayer();
+    }
   }
 });
 
 btnhold.addEventListener('click', function () {
-  //1.Add current score to active players score
-  scores[active] += currentscore;
-  document.getElementById(`score--${active}`).textContent = scores[active];
-  //2.check
+  if (playing) {
+    //1.Add current score to active players score
+    scores[active] += currentscore;
+    document.getElementById(`score--${active}`).textContent = scores[active];
 
-  //
+    //2.check if player's score >= 100
+    if (scores[active] >= 20) {
+      //Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${active}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${active}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      switchplayer();
+    }
+  }
+});
 
-  //
-  switchplayer();
+btnnew.addEventListener('click', function () {
+  //resetting all the scores current, score;
+  currentscore = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  //setting the state variable to true for playing
+  playing = true;
+
+  //resetting the score array to zeros;
+  scores[0] = 0;
+  scores[1] = 0;
+
+  //removing winner player black color and setting active player white color
+  document
+    .querySelector(`.player--${active}`)
+    .classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+
+  //changing active player to 0
+  active = 0;
 });
